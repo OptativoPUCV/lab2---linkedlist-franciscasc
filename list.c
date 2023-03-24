11e653bb3b81a1050c571348dc0ed3c232715c7c
Replit user <>
2023-03-24 18:00:11 +0000

    new try

--- list.c
+++ list.c
@@ -125,7 +125,7 @@ void * popCurrent(List * list) {
     list->current->next->prev = list->current->prev;
   }
   if(list->tail == list->current)
-    list->tail = list->current;
+    list->tail->prev = list->current;
   return (list->current->data);
 }
 void cleanList(List * list) {
