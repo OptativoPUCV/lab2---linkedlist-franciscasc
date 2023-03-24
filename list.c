1a070100d673777dd8d4adbebeb8a2e29a30ff27
Replit user <>
2023-03-24 14:20:59 +0000

    new try

--- list.c
+++ list.c
@@ -95,7 +95,7 @@ void pushCurrent(List * list, void * data) {
   if(list->current){
     list->current->next = nodo;
   }
-  list->tail->prev = nodo
+  list->tail->prev = nodo;
 }
 
 void * popFront(List * list) {
